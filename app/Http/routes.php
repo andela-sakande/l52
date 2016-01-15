a1c56f161dab1ca1add0dafbaa9f6e068a8102e6
andela-sakande <surajudeen.akande@andela.com>
2016-01-15 05:47:13 +0000

    (Feature) Add AdminLTE using bower

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,12 +26,12 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/home', 'HomeController@index');
+
+    Route::get('admin', function () {
+    return view('admin_template');
+});
 });
